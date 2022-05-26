e0a43d1132606195c76b61dc294ddd7ddd362e96
Hyunseo <gustj1109@naver.com>
2022-05-26 18:10:35 +0900

    F

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -32,7 +32,7 @@ public class MemberService {
             throw new IllegalStateException("이미 존재하는 회원입니다."); }
     }
     /**
-     *전체 회원 조회
+     *전체 회원 조회dddddd
      */
     public List<Member> findMembers() {
         return memberRepository.findAll();
